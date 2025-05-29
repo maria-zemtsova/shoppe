3ec293e267bca7bff76a59c71b3314b0a0b30cb6
maria-zemtsova <zemtsova.md@gmail.com>
2025-05-29 13:26:08 +0700

    scss has been added and adjusted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts']
+  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  css: ['~/assets/scss/main.scss']
 })
