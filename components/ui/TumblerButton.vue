6ff918b9482e04d1a2fdea631fff9a583130f60e
maria-zemtsova <zemtsova.md@gmail.com>
2025-08-10 16:22:26 +0700

    fix styles for tumbler button

--- components/ui/TumblerButton.vue
+++ components/ui/TumblerButton.vue
@@ -1,30 +1,35 @@
 <script lang="ts" setup>
-const props = defineProps<{
+  const props = defineProps<{
     modelValue: boolean
     text?: string
-}>()
+  }>()
 
-const emit = defineEmits<{
+  const emit = defineEmits<{
     (e: 'update:modelValue', value: boolean): void
-}>()
+  }>()
 
-const toggle = () => {
+  const toggle = () => {
     emit('update:modelValue', !props.modelValue)
-}
+  }
 </script>
 
 <template>
-    <div class="tumbler">
-        <p v-if="text" class="tumbler__text">{{ text }}</p>
-        <button class="tumbler__button" type="button" aria-label="Toggle switch"
-            :class="{ 'tumbler__button--active': props.modelValue }" @click="toggle">
-            <span class="tumbler__thumb" />
-        </button>
-    </div>
+  <div class="tumbler">
+    <p v-if="text" class="tumbler__text">{{ text }}</p>
+    <button
+      class="tumbler__button"
+      type="button"
+      aria-label="Toggle switch"
+      :class="{ 'tumbler__button--active': props.modelValue }"
+      @click="toggle"
+    >
+      <span class="tumbler__thumb" />
+    </button>
+  </div>
 </template>
 
 <style lang="scss">
-.tumbler {
+  .tumbler {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -32,47 +37,45 @@ const toggle = () => {
     margin-top: 40px;
 
     @media (max-width: $breakpoints-m) {
-        width: 288px;
+      width: 288px;
     }
 
     &__text {
-        margin: 0;
-        font-family: $font-dm-sans;
-        font-size: 16px;
-        line-height: 27px;
+      margin: 0;
+      font-family: $font-dm-sans;
+      font-size: 16px;
+      line-height: 27px;
+    }
+
+    &__thumb {
+      position: absolute;
+      top: 4px;
+      left: 4px;
+      width: 12px;
+      height: 12px;
+      background: $white;
+      border-radius: 50%;
+      box-shadow: 0 1px 2px rgb(0 0 0 / 20%);
+      transition: transform 0.3s ease;
     }
 
     &__button {
-        position: relative;
-        width: 34px;
-        height: 20px;
-        cursor: pointer;
-        background-color: $dark-gray;
-        border: none;
-        border-radius: 25px;
-        transition: background 0.3s ease;
+      position: relative;
+      width: 34px;
+      height: 20px;
+      cursor: pointer;
+      background-color: $dark-gray;
+      border: none;
+      border-radius: 25px;
+      transition: background 0.3s ease;
 
-        &--active {
-            background: $accent;
+      &--active {
+        background: $accent;
 
-            .tumbler__thumb {
-                transform: translateX(calc(#{28px} - #{12px} - 2px));
-            }
+        .tumbler__thumb {
+          transform: translateX(calc(#{28px} - #{12px} - 2px));
         }
+      }
     }
-
-
-
-    &__thumb {
-        position: absolute;
-        top: 4px;
-        left: 4px;
-        width: 12px;
-        height: 12px;
-        background: $white;
-        border-radius: 50%;
-        box-shadow: 0 1px 2px rgb(0 0 0 / 20%);
-        transition: transform 0.3s ease;
-    }
-}
+  }
 </style>
